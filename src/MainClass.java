e53d52feec9c099c585b1f2d7c7efccd3e97c6ce
Assad <Assad@172.16.105.112>
2016-05-30 17:47:52 +0500

    asd

--- src/MainClass.java
+++ src/MainClass.java
@@ -3,7 +3,9 @@ public class MainClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("Assad");
+		System.out.println("My name is");
+		System.out.println("Assad");
+
 	}
 
 }
---
+++ src/child.java
@@ -0,0 +1,5 @@
+
+public class child {
+int a;
+String name;
+}
